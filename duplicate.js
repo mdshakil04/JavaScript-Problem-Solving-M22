e9f19110c0be4c1411fafab63edb2e3bd1146319
mdshakil04 <m.shakil04@yahoo.com>
2024-02-09 20:20:50 +0600

    Duplicate Remove function

---
+++ duplicate.js
@@ -0,0 +1,9 @@
+const students = ["Mahfuz", "Tanvir", "Shifat", "Alvi", "Mahfuz", "Sumaiya", "Tanvir", "Shifat", "Alvi"]
+
+function removeDuplicate(name){
+    for(let i = 0; i < students.length; i++){
+        const student = students[i];
+        console.log(student)
+    }
+}
+const uniqueStudents = removeDuplicate(students);
