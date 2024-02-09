c379536d2aa23756b29930ad25f08c53e084428e
mdshakil04 <m.shakil04@yahoo.com>
2024-02-09 11:55:44 +0600

    Splice and Slice done

--- slice.js
+++ slice.js
@@ -1,7 +1,8 @@
 const marks = [12, 23, 23, 45, 34, 25, 67, 12]
-console.log("Original Array : ",marks)
-const arrSlice = marks.slice(3, 6);
+console.log("Original Array : ",marks);
+const arrSlice = marks.slice(3, 6); 
 console.log("Sliced Array : ", arrSlice);
+console.log("Original Array : ",marks);
 const arrSplice = marks.splice(3, 4);
 console.log("Spliced Array",arrSplice);
-console.log("Remaining Array : ",marks)
+console.log("Original Array : ",marks);
