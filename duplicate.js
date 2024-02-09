a2466db4f13c094ff7cb3c0a90954721a694cf09
mdshakil04 <m.shakil04@yahoo.com>
2024-02-09 21:15:22 +0600

    Another Problem Solved

--- duplicate.js
+++ duplicate.js
@@ -12,4 +12,18 @@ function removeDuplicate(name){
     return uniqueName;
 }
 const uniqueStudents = removeDuplicate(students);
-console.log(uniqueStudents)
+// console.log(uniqueStudents)
+// ---------------------------------------------------------------------------------
+const marks = [23, 24, 25, 26, 27, 27, 28, 29, 30, 24, 26, 29, 27, 28, 30]
+function duplicateRemover(number){
+    const uniqueNumber = [];
+    for(let i = 0; i < marks.length; i++){
+        const newNumber = marks[i];
+        if(uniqueNumber.includes(newNumber) === false){
+            uniqueNumber.push(newNumber);
+        }
+    }
+    return uniqueNumber;
+}
+const uniqueNumbers = duplicateRemover(marks)
+console.log(uniqueNumbers)
