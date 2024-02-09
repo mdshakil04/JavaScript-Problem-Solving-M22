6e0960c2084d1bcf79d0f5fbefeaee3413a69e09
mdshakil04 <m.shakil04@yahoo.com>
2024-02-09 20:41:51 +0600

    Code updated

--- duplicate.js
+++ duplicate.js
@@ -1,9 +1,15 @@
 const students = ["Mahfuz", "Tanvir", "Shifat", "Alvi", "Mahfuz", "Sumaiya", "Tanvir", "Shifat", "Alvi"]
 
 function removeDuplicate(name){
+    const uniqueName = [];
     for(let i = 0; i < students.length; i++){
         const student = students[i];
-        console.log(student)
+        // console.log(student)
+        if(uniqueName.includes(student) === false){
+            uniqueName.push(student)
+        }
     }
+    return uniqueName;
 }
 const uniqueStudents = removeDuplicate(students);
+console.log(uniqueStudents)
