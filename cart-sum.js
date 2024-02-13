5e90ffeb6b6db0742d20e39873d61a69a1514cf0
mdshakil04 <m.shakil04@yahoo.com>
2024-02-13 11:33:59 +0600

    Items quantity added

--- cart-sum.js
+++ cart-sum.js
@@ -1,16 +1,17 @@
 const phones = [
-    {name : "Samsung", Camera: 12, Storage: "32gb", price: 65000},
-    {name : "Nokia", Camera: 24, Storage: "8gb", price: 45000},
-    {name : "Xiaomi", Camera: 10, Storage: "12gb", price: 15000},
-    {name : "iPhone", Camera: 24, Storage: "8gb", price: 65000},
-    {name : "Techno", Camera: 64, Storage: "12gb", price: 5000}
+    {name : "Samsung", Camera: 12, Storage: "32gb", price: 65000, quantity: 2},
+    {name : "Nokia", Camera: 24, Storage: "8gb", price: 45000, quantity: 3},
+    {name : "Xiaomi", Camera: 10, Storage: "12gb", price: 15000, quantity: 4},
+    {name : "iPhone", Camera: 24, Storage: "8gb", price: 65000, quantity: 2},
+    {name : "Techno", Camera: 64, Storage: "12gb", price: 5000, quantity: 4}
 ];
 
 function sumCartItems(items){
     let sum = 0;
     for(let i = 0; i < items.length; i++){
         const item = items[i];
-        sum = sum + item.price;
+        const itemsTotal = item.price * item.quantity
+        sum = sum + itemsTotal;
     }
     return sum;
 }
